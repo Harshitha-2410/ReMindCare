93a618839d27092d7adc2f317ed30df5abce3b1c
Harshitha-2410 <harshithadasabohina@gmail.com>
2026-01-13 18:18:42 +0530

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,8 +1,17 @@
+# from django.apps import AppConfig
+# class AppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'app'
+# from django.apps import AppConfig
+
+# class AppConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'app'
+
+#     def ready(self):
+#         import app.signals
 from django.apps import AppConfig
-class AppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app'
-from django.apps import AppConfig
+
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
